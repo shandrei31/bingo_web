2c923675101a6d06b1c3f068e64e70caea319266
shandrei31 <dreishan7@gmail.com>
2024-03-18 08:51:11 +0800

    Bingooo

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ let B = false,
     N = false,
     G = false,
     O = false;
-function newNumber(maxNumber) {
+
+    let BNumber, INumber, NNumber, GNumber, ONumber;
+
+    function newNumber(maxNumber) {
     // Addition = "+"
     // Subtraction = "-"
     // Multiplication = "*"
@@ -18,23 +21,59 @@ function newNumber(maxNumber) {
     if (x <= 15) {
         console.log(`New number ${x} belongs to "B"`);
         B = true;
+        BNumber = x;
     } else if (x >= 16 && x <= 30) {
         console.log(`New number ${x} belongs to "I"`); 
         I = true;
+        INumber = x;
     } else if (x >=31 && x <=45) {
         console.log(`New number ${x} belongs to "N".`);
         N = true; 
+        NNumber = x;
     } else if (x >= 46 && x <= 60) { 
         console.log(`New number ${x} belongs to "G".`);
         G = true;
+        GNumber = x;
     } else if (x >=61 && x <=75) {
         console.log(`New number ${x} belongs to "O".`);
         O = true;
+        ONumber = x;
     }
 
      if (B == true && I == true && N == true && G == true && O == true) {
         console.log(`BINGOOO!!!`);
+        console.table({ 
+        B: BNumber,
+        I: INumber,
+        N: NNumber,
+        G: GNumber,
+        O: ONumber,
+    });
+        
+             
     }
 }
 
 newNumber(75);
+
+// LOOPS
+ let counter = 5;
+// for (counter; counter >= 0; counter --) {
+   // console.log(`Current counter is ${counter}`);
+
+    // if (counter == 2) break;
+    
+   // newNumber(75);
+    
+// }
+
+// while (counter >= 0) {
+   // console.log(`While, Current counter is ${counter}`);
+  // counter--;
+ // }
+ 
+ while (B == false ||  I == false || N == false ||  G == false ||  O == false ) {
+  newNumber(75);
+ }
+
+
